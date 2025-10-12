ae89d8c3a0f903efb46ff8bde4cecb6f783a50af
18618287247 <18683731@qq.com>
2025-10-12 19:49:13 +0800

    增加，springboot 加载支持

--- src/main/java/net/ooder/esd/custom/component/form/field/CustomFieldFormComponent.java
+++ src/main/java/net/ooder/esd/custom/component/form/field/CustomFieldFormComponent.java
@@ -1,21 +1,25 @@
 package net.ooder.esd.custom.component.form.field;
 
 import com.alibaba.fastjson.annotation.JSONField;
-import net.ooder.context.JDSActionContext;
+import net.ooder.esd.annotation.action.CustomFormAction;
 import net.ooder.esd.annotation.ui.ComponentType;
-import net.ooder.esd.annotation.ui.PositionType;
-import net.ooder.esd.bean.field.CustomFormLayoutFieldBean;
-import net.ooder.esd.bean.view.CustomFormViewBean;
+import net.ooder.esd.annotation.ui.RequestPathTypeEnum;
+import net.ooder.esd.annotation.ui.ResponsePathTypeEnum;
 import net.ooder.esd.bean.MethodConfig;
+import net.ooder.esd.bean.field.CustomFormLayoutFieldBean;
 import net.ooder.esd.bean.field.combo.ComboBoxBean;
 import net.ooder.esd.bean.field.combo.ComboInputFieldBean;
+import net.ooder.esd.bean.view.CustomFormViewBean;
 import net.ooder.esd.custom.CustomViewConfigFactory;
+import net.ooder.esd.custom.component.form.FormLayoutModule;
 import net.ooder.esd.dsm.view.field.FieldFormConfig;
 import net.ooder.esd.engine.EUModule;
+import net.ooder.esd.tool.component.APICallerComponent;
 import net.ooder.esd.tool.component.Component;
 import net.ooder.esd.tool.component.FormLayoutComponent;
+import net.ooder.esd.tool.properties.APICallerProperties;
+import net.ooder.esd.tool.properties.UrlPathData;
 import net.ooder.esd.tool.properties.form.FormLayoutProperties;
-import ognl.OgnlException;
 
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
@@ -35,14 +39,13 @@ public class CustomFieldFormComponent extends FormLayoutComponent {
         //存在集合属性
         if (field.getMethodConfig() != null && field.getMethodConfig().getView() != null && field.getMethodConfig().getView() instanceof CustomFormViewBean) {
             CustomFormViewBean customComponentViewBean = (CustomFormViewBean) field.getMethodConfig().getView();
+
             init(euModule, customComponentViewBean, valueMap);
-            FormLayoutProperties blockProperties = new FormLayoutProperties(customComponentViewBean,field.getWidgetConfig());
+
+            FormLayoutProperties blockProperties = new FormLayoutProperties(customComponentViewBean, field.getWidgetConfig());
             this.setProperties(blockProperties);
             this.setTarget(target);
-
         }
-
-
         Component component = field.getWidgetConfig().getComponent();
         if (component != null) {
             if (component.getCS() != null) {
@@ -56,7 +59,6 @@ public class CustomFieldFormComponent extends FormLayoutComponent {
 
 
     public CustomFieldFormComponent(EUModule euModule, MethodConfig methodConfig, Map dbMap) {
-
         CustomFormViewBean customComponentViewBean = (CustomFormViewBean) methodConfig.getView();
         FormLayoutProperties blockProperties = new FormLayoutProperties();
         this.setProperties(blockProperties);
@@ -66,91 +68,40 @@ public class CustomFieldFormComponent extends FormLayoutComponent {
 
 
     void init(EUModule euModule, CustomFormViewBean customComponentViewBean, Map dbMap) {
-        List<FieldFormConfig> formFieldList = customComponentViewBean.getAllFields();
-        this.euModule = euModule;
-        List<FieldFormConfig> fieldList = new ArrayList<>();
-        for (FieldFormConfig fieldFormConfig : formFieldList) {
-            if ((fieldFormConfig.getColHidden() == null || !fieldFormConfig.getColHidden())  && !fieldFormConfig.getComponentType().equals(ComponentType.HIDDENINPUT)) {
-                fieldList.add(fieldFormConfig);
-            }
-        }
-        if (dbMap != null) {
-            this.valueMap = dbMap;
-        }
-        for (FieldFormConfig fieldInfo : fieldList) {
-
-            Component inputComponent = null;
-
-            Class fieldClass = fieldInfo.getEsdField().getReturnType();
-            if (Component.class.isAssignableFrom(fieldClass)) {
-
-                if (fieldInfo.getMethodConfig()!=null && fieldInfo.getMethodConfig().getRequestMethodBean()!=null){
-                    try {
-                        inputComponent = (Component)fieldInfo.getMethodConfig().getRequestMethodBean().invok(JDSActionContext.getActionContext().getOgnlContext(), JDSActionContext.getActionContext().getContext());
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (OgnlException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-
-            } else {
-                ComponentType componentType = fieldInfo.getComponentType();
-                if (fieldInfo.getComboConfig() != null && (fieldInfo.getWidgetConfig() instanceof ComboInputFieldBean) && !componentType.equals(ComponentType.MODULE)) {
-                    componentType = ComponentType.COMBOINPUT;
-                }
-                Object value = null;
-                if (valueMap != null) {
-                    value = valueMap.get(fieldInfo.getFieldname());
-                    valueMap.put("parent", this);
-                }
-
-                switch (componentType) {
-                    case COMBOINPUT:
-                        ComboBoxBean comboBoxBean = fieldInfo.getComboConfig();
-                        Class<Component> defaultFieldClass = CustomViewConfigFactory.getInstance().getComboBoxComponent(comboBoxBean.getInputType());
-                        try {
-                            if (defaultFieldClass != null) {
-                                Constructor<Component> constructor = defaultFieldClass.getConstructor(new Class[]{EUModule.class, FieldFormConfig.class, String.class, Object.class, Map.class});
-                                inputComponent = constructor.newInstance(euModule, fieldInfo, null, value, valueMap);
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        break;
-
-                    default:
-                        defaultFieldClass = CustomViewConfigFactory.getInstance().getComponent(componentType);
-                        if (defaultFieldClass != null) {
-                            try {
-                                if (defaultFieldClass != null) {
-                                    Constructor<Component> constructor = defaultFieldClass.getConstructor(new Class[]{EUModule.class, FieldFormConfig.class, String.class, Object.class, Map.class});
-                                    inputComponent = constructor.newInstance(euModule, fieldInfo, null, value, valueMap);
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        } else {
-                            inputComponent = new CustomHiddenInputComponent(euModule, fieldInfo, null, value, valueMap);
-                        }
-                }
-            }
-            if (inputComponent.getKey().equals(ComponentType.MQTT.getClassName())) {
-                this.euModule.getComponent().addChildren(inputComponent);
-            } else if (inputComponent.getKey().equals(ComponentType.APICALLER.getClassName())) {
-                this.euModule.getComponent().addChildren(inputComponent);
-            } else {
-
-                PositionType positionType = fieldInfo.getFieldBean().getInnerPosition();
-                if (positionType == null || positionType.equals(PositionType.inner)) {
-                    this.addChildren(inputComponent);
-                } else {
-                    this.euModule.getComponent().addChildren(inputComponent);
-                }
-            }
+        FormLayoutModule layoutModule = new FormLayoutModule(euModule, this, customComponentViewBean.getAllFields(), dbMap, customComponentViewBean);
+        MethodConfig methodConfig = euModule.getComponent().getMethodAPIBean();
+        if (methodConfig != null) {
+            genAPIComponent(euModule.getComponent().getCtxBaseComponent(), methodConfig, this);
         }
     }
+
+
+    //数据对象
+    @JSONField(serialize = false)
+    APICallerComponent[] genAPIComponent(Component ctxComponent, MethodConfig methodConfig, FormLayoutComponent mainComponent) {
+        List<APICallerComponent> apiCallerComponents = new ArrayList<APICallerComponent>();
+        APICallerComponent reloadAPI = new APICallerComponent(methodConfig);
+        if (reloadAPI != null) {
+            reloadAPI.setAlias(CustomFormAction.RELOAD.getTarget());
+            //刷新调用
+            APICallerProperties reloadProperties = reloadAPI.getProperties();
+            UrlPathData treepathData = new UrlPathData(mainComponent.getAlias(), RequestPathTypeEnum.FORM, "");
+            reloadProperties.addRequestData(treepathData);
+
+            UrlPathData ctxData = new UrlPathData(ctxComponent.getAlias(), RequestPathTypeEnum.FORM, "");
+            reloadProperties.addRequestData(ctxData);
+
+            UrlPathData formData = new UrlPathData(mainComponent.getAlias(), ResponsePathTypeEnum.FORM, "data");
+            reloadProperties.addResponseData(formData);
+
+            UrlPathData formCtxData = new UrlPathData(ctxComponent.getAlias(), ResponsePathTypeEnum.FORM, "data");
+            reloadProperties.addResponseData(formCtxData);
+            apiCallerComponents.add(reloadAPI);
+        }
+
+        return apiCallerComponents.toArray(new APICallerComponent[]{});
+    }
+
 
     private Component createComponent(FieldFormConfig fieldInfo, String target, Object value, Map valueMap) {
         Component inputComponent = null;
