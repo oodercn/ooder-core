42c5727315eafa581f4337e95e80288408d12d37
18618287247 <18683731@qq.com>
2025-10-15 13:46:48 +0800

    增加，springboot 加载支持

--- src/main/java/net/ooder/esd/tool/component/FoldingListComponent.java
+++ src/main/java/net/ooder/esd/tool/component/FoldingListComponent.java
@@ -1,29 +1,27 @@
 package net.ooder.esd.tool.component;
 
-import net.ooder.esd.annotation.event.FoldingListEventEnum;
+import net.ooder.esd.annotation.event.ListEventEnum;
 import net.ooder.esd.annotation.ui.ComponentType;
 import net.ooder.esd.tool.properties.Action;
 import net.ooder.esd.tool.properties.list.ListFieldProperties;
 
 
-public class FoldingListComponent<T extends ListFieldProperties, K extends FoldingListEventEnum> extends Component<T, K> {
+public class FoldingListComponent extends FieldComponent<ListFieldProperties, ListEventEnum> {
 
-
-
-    public FoldingListComponent v(Action<K> action) {
+    public FoldingListComponent addAction(Action<ListEventEnum> action) {
         super.addAction(action);
         return this;
     }
 
-    public FoldingListComponent(String alias, T properties) {
-        super(ComponentType.LIST, alias);
+    public FoldingListComponent(String alias, ListFieldProperties properties) {
+        super(ComponentType.FOLDINGLIST, alias);
         this.setProperties(properties);
 
     }
 
     public FoldingListComponent() {
-        super(ComponentType.LIST);
-        this.setProperties((T) new ListFieldProperties());
+        super(ComponentType.FOLDINGLIST);
+        this.setProperties(new ListFieldProperties());
     }
 
 
