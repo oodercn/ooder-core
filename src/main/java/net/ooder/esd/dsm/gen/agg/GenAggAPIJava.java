b2d8a2f8879686ab43c8d4222c03e3deedba6fe4
18618287247 <18683731@qq.com>
2025-11-20 20:54:30 +0800

    增加，springboot 加载支持

--- src/main/java/net/ooder/esd/dsm/gen/agg/GenAggAPIJava.java
+++ src/main/java/net/ooder/esd/dsm/gen/agg/GenAggAPIJava.java
@@ -1,32 +1,24 @@
 package net.ooder.esd.dsm.gen.agg;
 
+import net.ooder.annotation.AggregationType;
 import net.ooder.common.logging.ChromeProxy;
-import net.ooder.common.util.StringUtility;
 import net.ooder.context.JDSActionContext;
-import net.ooder.context.JDSContext;
 import net.ooder.esd.dsm.BuildFactory;
-import net.ooder.esd.dsm.DSMFactory;
 import net.ooder.esd.dsm.aggregation.AggEntityConfig;
 import net.ooder.esd.dsm.aggregation.DomainInst;
-import net.ooder.esd.dsm.aggregation.context.AggEntityRoot;
 import net.ooder.esd.dsm.aggregation.context.AggProxyRoot;
-import net.ooder.esd.dsm.aggregation.ref.AggEntityRef;
 import net.ooder.esd.dsm.enums.RangeType;
-import net.ooder.esd.dsm.gen.GenJavaTask;
 import net.ooder.esd.dsm.gen.GenJava;
+import net.ooder.esd.dsm.gen.GenJavaTask;
 import net.ooder.esd.dsm.java.JavaSrcBean;
 import net.ooder.esd.dsm.temp.JavaTemp;
-import net.ooder.server.context.MinServerActionContextImpl;
-import net.ooder.annotation.AggregationType;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.Callable;
 
-public class GenAggAPIJava  extends GenJavaTask {
+public class GenAggAPIJava extends GenJavaTask {
 
     DomainInst domainInst;
     AggEntityConfig esdClassConfig;
