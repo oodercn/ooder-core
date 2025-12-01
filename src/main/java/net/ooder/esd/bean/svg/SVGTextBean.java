a18269f692a21868ac753572fbb11b5fe17f669a
18618287247 <18683731@qq.com>
2025-12-01 11:00:23 +0800

    重构SVG支持

--- src/main/java/net/ooder/esd/bean/svg/SVGTextBean.java
+++ src/main/java/net/ooder/esd/bean/svg/SVGTextBean.java
@@ -77,7 +77,9 @@ public class SVGTextBean implements FieldComponentBean<SVGTextComponent> {
     void init(SVGTextProperties properties) {
         Map valueMap = JSON.parseObject(JSON.toJSONString(properties), Map.class);
         OgnlUtil.setProperties(valueMap, this, false, false);
-        OgnlUtil.setProperties(JSON.parseObject(JSON.toJSONString(properties.getAttr()), Map.class), this, false, false);
+        if (properties.getAttr()!=null){
+            JSON.parseObject(JSON.toJSONString(properties.getAttr()),this.getClass());
+        }
     }
 
     @Override
